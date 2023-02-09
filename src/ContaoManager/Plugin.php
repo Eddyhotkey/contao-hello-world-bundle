9bf4f7d777003bc19f4cde1fa2af02204e4fa14f
Eddyhotkey <rshdbairamov@gmail.com>
2023-02-09 11:13:25 +0100

    file has been changed

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -23,7 +23,7 @@ class Plugin implements BundlePluginInterface
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(ContaoSkeletonBundle::class)
+            BundleConfig::create(ContaoHelloWorldBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
