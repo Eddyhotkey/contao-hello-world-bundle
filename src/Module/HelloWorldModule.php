6cd35f63f45509fe196d20c8749712620b416e4c
Eddyhotkey <rshdbairamov@gmail.com>
2023-02-09 16:43:41 +0100

    Use Haste to generate HelloWorldModule wildcard

--- src/Module/HelloWorldModule.php
+++ src/Module/HelloWorldModule.php
@@ -2,9 +2,7 @@
 
 namespace Eddyhotkey\ContaoHelloWorldBundle\Module;
 
-use Eddyhotkey\ContaoHelloWorldBundle\Library\MessageGenerator;
-
-class HelloWorldModule extends \Module
+class HelloWorldModule extends \Haste\Frontend\AbstractFrontendModule
 {
   /**
    * @var string
@@ -12,33 +10,11 @@ class HelloWorldModule extends \Module
   protected $strTemplate = 'mod_helloWorld';
 
   /**
-   * Displays a wildcard in the back end.
-   *
-   * @return string
-   */
-  public function generate()
-  {
-    if ( TL_MODE == 'BE') {
-      $template = new \BackendTemplate( 'be_wildcard' );
-
-      $template->wildcard = '### '.utf8_strtoupper( $GLOBALS[ 'TL_LANG' ][ 'FMD' ][ 'helloWorld' ][ 0 ]). ' ###';
-      $template->title = $this->headline;
-      $template->id = $this->id;
-      $template->link = $this->name;
-      $template->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id='.$this->id;
-
-      return $template->parse();
-    }
-
-    return parent::generate();
-  }
-
-  /**
    * Generates the module.
    */
   protected function compile()
   {
-    $messageGenerator = \Contao\System::getContainer()->get(MessageGenerator::class);
+    $messageGenerator = \Contao\System::getContainer()->get('eddyhotkey.contao_hello_world_bundle.message_generator');
 
     $message = $messageGenerator->sayHelloTo('World');
 
